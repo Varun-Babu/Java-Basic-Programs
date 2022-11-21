cd683802f5ef433306d0df461c46792f561a6a39
Varun-Babu <itsme.varun.26@gmail.com>
2022-11-21 10:32:08 +0530

    subtraction added

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,13 @@ public class Main {
         double sum = i+j;
         return sum;
     }
+    //    SUB
+    public static double sub(double i, double j){
+        double sub = i-j;
+        return sub;
+    }
     public static void main(String[] args) {
         System.out.println("Ready to Calculate!!!");
+
     }
 }
